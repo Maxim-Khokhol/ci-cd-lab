352da9de981ae1f50a7324fe7f1a2c2cc44695a1
Maxim-Khokhol <khokhol.maxim@lll.kpi.ua>
2025-04-09 01:03:03 +0300

    Added main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region     = "eu-north-1"
+  }
+
+resource "aws_instance" "lab_server" {
+  ami           = "ami-0989fb15ce71ba39e"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-lab"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update
+              sudo apt install -y apache2
+              sudo systemctl start apache2
+              sudo systemctl enable apache2
+              EOF
+
+  vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
+}
+
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Allow HTTP and SSH traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
